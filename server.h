8c4279366de329d2ada158d7025a742eb78ba721
Brian Yarbrough <brian@idealwebtech.com>
2011-11-11 12:11:54 -0600

    My current changes

--- server.h
+++ server.h
@@ -1,10 +1,11 @@
 #ifndef SERVER_H
 #define SERVER_H
-#include <cstdlib>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
 
+const unsigned short DEFAULT_PORT = 1323;
+const unsigned short MAX_CLIENTS = 2;
+
+void signalHandler(int sig);
+void * handleClient(void * ptr);
+void writeToAll(const char content[256]);
 
 #endif
