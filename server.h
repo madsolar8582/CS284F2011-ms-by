1cdf88b2ea5b9d0dc57b37c0f7178f9bb6c0fa12
Brian Yarbrough <brian@idealwebtech.com>
2011-11-11 15:23:01 -0600

    Fixed the issue causing the server not to see messages from the client

--- server.h
+++ server.h
@@ -1,10 +1,11 @@
+
 #ifndef SERVER_H
 #define SERVER_H
 
 const unsigned short DEFAULT_PORT = 1323;
 const unsigned short MAX_CLIENTS = 2;
 
-void signalHandler(int sig);
+void signalHandler(int signal);
 void * handleClient(void * ptr);
 void writeToAll(const char content[256]);
 
