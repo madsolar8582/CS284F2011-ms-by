12faeebc7518d2476ebfb28121066e08d8654c26
Brian Yarbrough <brian@idealwebtech.com>
2011-11-12 13:17:20 -0600

    Finished Server Code (Has all requirements fulfilled)

--- server.h
+++ server.h
@@ -1,12 +1,55 @@
+/***
+|* Created by Brian Yarbrough & Madison Solarana
+|* Created on 11/10/2011
+|* Last Modified on 11/12/2011
+***/
 
 #ifndef SERVER_H
 #define SERVER_H
 
-const unsigned short DEFAULT_PORT = 1323;
-const unsigned short MAX_CLIENTS = 2;
+// ### GLOBAL CONSTANTS ########################################################
+const unsigned short DEFAULT_PORT = 1300;
+const unsigned short MAX_CLIENTS = 10;
 
+// ### FUNCTION DECLARATIONS ###################################################
+/***
+|* @func  signalHandler
+|* @desc	Handles abort, terminate & interupt signals.
+|*
+|* @param	int		signal		The signal the script recieved.
+***/
 void signalHandler(int signal);
+
+/***
+|* @func	handleClient
+|* @desc	Handles a new client. (For use with pthread_create only!)
+***/
 void * handleClient(void * ptr);
-void writeToAll(const char content[256]);
+
+/***
+|* @func	writeToAll
+|* @desc	Writes a message to all clients.
+|*
+|* @param	char[]	message		The message to write to all clients.
+***/
+void writeToAll(const char message[]);
+
+/***
+|* @func	writeToAllOthers
+|* @desc	Writes a message to all clients except the one the message is from.
+|*
+|* @param	char[]			message		The message to write to all clients.
+|* @param	unsigned short	from		The client this message is from.
+***/
+void writeToAllOthers(const char message[], unsigned short from);
+
+/***
+|* @func	writeMessage
+|* @desc	Formats a message before writing it to all clients except the one the message is from.
+|*
+|* @param	char[]			message		The message to write to all clients.
+|* @param	unsigned short	from		The client this message is from.
+***/
+void writeMessage(const char message[], unsigned short from);
 
 #endif
