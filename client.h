46129ded52006ebc61a617789338381a8c88bcc7
Brian Yarbrough <brian@idealwebtech.com>
2011-11-12 18:09:42 -0600

    Mostly Done Client

--- client.h
+++ client.h
@@ -1,10 +1,27 @@
+/***
+|* Created by Brian Yarbrough & Madison Solarana
+|* Created on 11/12/2011
+***/
+
 #ifndef CLIENT_H
 #define CLIENT_H
-#include <cstdlib>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
 
+// ### GLOBAL CONSTANTS ########################################################
+const unsigned short DEFAULT_PORT = 1300;
+
+// ### FUNCTION DECLARATIONS ###################################################
+/***
+|* @func  interuptHandler
+|* @desc	Handles interupt signals.
+|*
+|* @param	int		signal		The signal the script recieved.
+***/
+void interuptHandler(int signal);
+
+/**
+|*	@func	about
+|*	@desc	Displays information about the program.
+**/
+void about();
 
 #endif
